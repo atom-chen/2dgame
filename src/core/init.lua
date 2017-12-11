a4bcd14acdc71699f7914f1fc56f53ff9c4caf0e
preboy.sl <preboy@126.com>
2017-12-11 10:25:17 +0800

    new protobuf.lua file

---
+++ src/core/init.lua
@@ -0,0 +1,7 @@
+
+require "core.protobuf"
+require "core.net_events"
+
+require "core.AnimLoader"
+require "core.WinBase"
+require "core.WinManager"
--- src/main.lua
+++ src/main.lua
@@ -4,6 +4,8 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 require "config"
 require "cocos.init"
 require "base.init"
+require "core.init"
+
 
 -- zcg.logTable(debug.getregistry(), "e:/reg.txt")
 
