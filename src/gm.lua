fe676adf51144a00b0a2c5abb0286250721e7c8a
Preboy.ZHANG <preboy@126.com>
2018-03-01 21:18:29 +0800

    * gm file : lua syntax error

--- src/gm.lua
+++ src/gm.lua
@@ -5,9 +5,9 @@ zcg.logTable(cc.Scheduler, "e:/a.txt")
 
 local gm_str = 
 {
-    [1] = "save"                    -- 保存数据
-    [2] = "item 4001|1 4002|2"      -- 增加道具 道具ID|数量 
-    [3] = "hero 5001 5002"          --  增加英雄 英雄ID
+    [1] = "save",                       -- 保存数据
+    [2] = "item 4001|1 4002|2",         -- 增加道具 道具ID|数量 
+    [3] = "hero 5001 5002",             --  增加英雄 英雄ID
 }
 
 MSG.SendGMCommand(gm_str[1])
