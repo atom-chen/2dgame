91c3ad3bada3d3cd23b90adbfc4000187a9d78af
preboy.sl <preboy@126.com>
2018-03-01 14:44:30 +0800

    gm

--- src/gm.lua
+++ src/gm.lua
@@ -3,14 +3,15 @@
 zcg.logTable(cc.Scheduler, "e:/a.txt")
 
 
--- 增加道具
-local gm_str = "additem 4001|1 4002|2"      -- 道具ID|数量 
+local gm_str = 
+{
+    [1] = "save"                    -- 保存数据
+    [2] = "item 4001|1 4002|2"      -- 增加道具 道具ID|数量 
+    [3] = "hero 5001 5002"          --  增加英雄 英雄ID
+}
 
--- 增加英雄
--- local gm_str = "additem 5001 5002"          --  英雄ID
+MSG.SendGMCommand(gm_str[1])
 
 
--- MSG.SendGMCommand(gm_str)
-
-MSG.SendNotice("fuck you, can i ?")
+-- MSG.SendNotice("fuck you, can i ?")
 
