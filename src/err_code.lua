44134c202379e7c98726893e3f4b4321f1528f35
preboy.sl <preboy@126.com>
2018-03-22 12:38:36 +0800

    new file: error code

---
+++ src/err_code.lua
@@ -0,0 +1,10 @@
+
+cc.exports.Error = {
+	ERR_OK                  = 0,    -- 无错误
+	ERR_FAILED              = 1,    -- 操作失败
+	ERR_LOGIN_FAILED        = 2,    -- 用户名、密码不正确
+	ERR_LOGIN_DUP           = 3,    -- 已在线，重复登录
+	ERR_NOT_LOGIN           = 4,    -- 未登录
+	ERR_END
+}
+
