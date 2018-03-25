5dba5bb6cacab9cfd62deecc9c1c868803469de7
Preboy.ZHANG <preboy@126.com>
2018-03-25 19:44:39 +0800

    gm

--- src/gm.lua
+++ src/gm.lua
@@ -1,7 +1,10 @@
-
-
 -- press 'z' will do this file
+
 -- zcg.logTable(cc.Scheduler, "d:/a.txt")
+
+
+-- zcg.logTable(debug.getregistry(), "d:/reg.txt")
+-- print("+++", type(cc.Node), cc.Node[".isclass"])
 
 
 local gm_str =
@@ -9,11 +12,9 @@ local gm_str =
     [1] = "save",                       -- 保存数据
     [2] = "item 4001|1 4002|2",         -- 增加道具 道具ID|数量
     [3] = "hero 5001 5002",             --  增加英雄 英雄ID
+    [4] = "hero_up 5001 5002",             --  增加英雄 英雄ID
+
 }
-
-
--- zcg.logTable(debug.getregistry(), "d:/reg.txt")
--- print("+++", type(cc.Node), cc.Node[".isclass"])
 
 
 -- MSG.SendGMCommand(gm_str[1])
@@ -22,8 +23,6 @@ local gm_str =
 -- MSG.SendNotice("fuck you, can i ?")
 
 
-
-Socket.SendPacket(Opcode.MSG_CS_MakeBattle, {
-    id = 1,
-})
+-- 打一架
+Socket.SendPacket(Opcode.MSG_CS_MakeBattle, { id = 1, })
 
