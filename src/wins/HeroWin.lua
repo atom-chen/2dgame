c596e6b7de9690ee74104bd9309c5a8d967786b5
preboy.sl <preboy@126.com>
2018-03-29 15:04:11 +0800

    add icon

--- src/wins/HeroWin.lua
+++ src/wins/HeroWin.lua
@@ -4,7 +4,15 @@ local HeroWin       = class("HeroWin", WinBase)
 
 function HeroWin:ctor()
     WinBase.ctor(self)
-    
+    -- 关闭按钮
+    local btn_image = ccui.Scale9Sprite:create("unKnown.png")
+    local btn_close = cc.ControlButton:create(btn_image)
+    btn_close:setPreferredSize(btn_image:getPreferredSize())
+    btn_close:setPosition(cc.p(525, 275))
+    self:addChild(btn_close)
+    btn_close:registerControlEventHandler(function()
+        WinManager:DestroyWindow(self)
+    end, 32)
 
 
 end
