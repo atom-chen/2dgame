809106343a6591fa892d1a018aa0dccc0b6663f0
zcg <preboy@126.com>
2019-01-15 17:47:35 +0800

    tidy

--- src/core/armature.lua
+++ src/core/armature.lua
@@ -76,9 +76,7 @@ function Armature:onTouchMoved(touch, event)
 end
 
 function Armature:onTouchEnded(touch, event)
-    if self.on_touch then
-        self.on_touch()
-    end
+    _ = self.on_touch and self.on_touch()
 end
 
 
